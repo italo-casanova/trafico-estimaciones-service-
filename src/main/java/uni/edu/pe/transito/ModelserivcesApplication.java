e8a1626c0c7192d0fb86d434a066dbbd977aecfb
Italo <italo.casanova.d@uni.pe>
2023-11-21 21:31:39 -0500

    first commit

---
+++ src/main/java/uni/edu/pe/transito/ModelserivcesApplication.java
@@ -0,0 +1,13 @@
+package uni.edu.pe.transito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelserivcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelserivcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/edu/pe/transito/ModelserivcesApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.edu.pe.transito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelserivcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
